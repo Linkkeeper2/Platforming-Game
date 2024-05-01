ec0a8a5b2a66fd500cde92e32f11af94a44e7059
Jacob Morello <alternationgd57@gmail.com>
2024-05-01 10:02:19 -0400

    Added libraries and updated settings

---
+++ src/main/java/screen/RoomScreen.java
@@ -0,0 +1,11 @@
+package main.java.screen;
+
+public class RoomScreen extends GameScreen {
+    public RoomScreen(int level) {
+
+    }
+
+    private void renderLevel(int level) {
+
+    }
+}
--- src/main/java/screen/gui/Start.java
+++ src/main/java/screen/gui/Start.java
@@ -1,10 +1,10 @@
 package main.java.screen.gui;
 
 import main.java.MyGame;
-import main.java.screen.GameScreen;
+import main.java.screen.RoomScreen;
 
 public class Start implements ButtonAction {
     public void action() {
-        MyGame.screen = new GameScreen();
+        MyGame.screen = new RoomScreen(0);
     }
 }
