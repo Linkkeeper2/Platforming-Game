05ace7fbc21d7b0cb88ee63d4aafe88818f72ca2
Jacob Morello <alternationgd57@gmail.com>
2024-06-07 08:08:30 -0400

    Added error handling for ServerUpdates

--- src/main/java/server/ServerUpdates.java
+++ src/main/java/server/ServerUpdates.java
@@ -7,13 +7,17 @@ import main.java.screen.GameScreen;
 public class ServerUpdates extends Thread {
     public void run() {
         while (MyGame.screen instanceof GameScreen) {
-            MyGame.database.updateMain();
-            Player.updatePlayers();
-            MyGame.database.addPlayers();
-            MyGame.database.updateMessages();
             try {
-                sleep(10);
-            } catch (InterruptedException e) {
+                MyGame.database.updateMain();
+                Player.updatePlayers();
+                MyGame.database.addPlayers();
+                MyGame.database.updateMessages();
+                try {
+                    sleep(10);
+                } catch (InterruptedException e) {
+                }
+            } catch (Exception e) {
+                MyGame.status.addMessage("Could not recieve data from server", 5000);
             }
         }
     }
